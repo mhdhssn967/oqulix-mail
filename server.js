d8ba5d310c3fd062bfb6a9532e9a25fad24036ed
mhdhssn <mhdhssn967@gmail.com>
2025-08-21 18:08:13 +0530

    server ix

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.post("/send-email", async (req, res) => {
 });
 
 // 🚀 Start the server
-app.listen(3001, () => {
-  console.log("Server is running");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
